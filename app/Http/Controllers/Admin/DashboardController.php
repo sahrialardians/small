9b05d39cc302653b78bdc17d4c849411b5e03b14
sahrialardians <sahrialardians@gmail.com>
2020-05-02 06:08:46 +0700

    create dashboard controller

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,11 +4,18 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Article;
+use App\Models\Comment;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('pages.admin.dashboard');
+        return view('pages.admin.dashboard', [
+            'users' => User::count(),
+            'articles' => Article::count(),
+            'comments' => Comment::count()
+        ]);
     }
 }
