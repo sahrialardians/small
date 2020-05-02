c7bd30ff268062c1228e70815a9d85f13c63f6f9
sahrialardians <sahrialardians@gmail.com>
2020-05-03 00:32:10 +0700

    create relationship one-to-many coments, tags

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -1,10 +1,23 @@
 <?php
 
 namespace App\Models;
+use App\Models\Comment;
+use App\Models\Tag;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    //
+    protected $guarded = [];
+    
+    public function comments()
+    {
+        return $this->hasMany(Comment::class, 'article_id', 'id');
+    }
+
+    public function tags()
+    {
+        return $this->hasMany(Tag::class, 'tag_id', 'id');
+    }
 }
+
