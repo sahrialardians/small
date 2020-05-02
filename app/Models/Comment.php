4f52c5cbe1aa9035abed102e55da68236482ea0d
sahrialardians <sahrialardians@gmail.com>
2020-05-03 00:32:56 +0700

    create relationship article, user

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -1,10 +1,22 @@
 <?php
 
-namespace App;
+namespace App\Models;
+use App\Models\Article;
+use App\Models\User;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    protected $guaded = [];
+
+    public function article()
+    {
+        return $this->belongsTo(Article::class, 'article_id', 'id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
 }
