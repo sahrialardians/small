b122f75ff4e916da8ea54c0c94bfef9ac508fec6
sahrialardians <sahrialardians@gmail.com>
2020-05-03 00:31:16 +0700

    create relationship many-to-many

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -1,10 +1,16 @@
 <?php
 
-namespace App;
+namespace App\Models;
+use App\Models\Article;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    //
+    protected $guarded = [];
+
+    public function articleTags()
+    {
+        return $this->belongsToMany(Article::class, 'article_tags', 'article_id', 'tag_id');
+    }
 }
