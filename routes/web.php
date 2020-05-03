e4c154b6d827f19876ce1948456df55bed180939
sahrialardians <sahrialardians@gmail.com>
2020-05-03 22:44:16 +0700

    change route

--- routes/web.php
+++ routes/web.php
@@ -12,16 +12,30 @@
 */
 
 Route::resource('/', 'HomeController');
+Route::resource('/profile', 'ProfileController')
+    ->middleware(['auth']);
+Route::resource('/articles', 'ArticleController')
+    ->middleware(['auth']);
 
 // untuk pisahin struktur admin
-Route::prefix('admin')
-    ->namespace('Admin')
-    ->middleware(['auth', 'admin'])
-    ->group(function(){
-        Route::get('/', 'DashboardController@index')
-            ->name('dashboard');
-});
+// Route::prefix('admin')
+//     ->namespace('Admin')
+//     ->middleware(['auth', 'admin'])
+//     ->group(function(){
+//         Route::get('/', 'DashboardController@index')
+//             ->name('dashboard');
+
+//         Route::resource('/users', 'UserController');
+//         Route::resource('/articles', 'ArticleController');
+//         Route::resource('/comments', 'CommentController');
+// });
 
 
+Route::get('/admin', 'Admin\DashboardController@index')
+    ->name('dashboard')->middleware(['auth', 'admin']);
+
+Route::resource('/admin/users', 'Admin\UserController')->middleware(['auth', 'admin']);
+// Route::resource('/admin/articles', 'Admin\ArticleController')->middleware(['auth', 'admin']);
+Route::resource('/admin/comments', 'Admin\CommentController')->middleware(['auth', 'admin']);
 
 Auth::routes(['verify' => true]);
