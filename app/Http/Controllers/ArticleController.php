88d518b4efa56c70eeddd0ef38fa340df6ec422b
sahrialardians <sahrialardians@gmail.com>
2020-05-03 22:37:27 +0700

    create function read, store and edit

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -1,6 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Article;
+use App\Models\User;
 
 use Illuminate\Http\Request;
 
@@ -13,7 +16,8 @@ class ArticleController extends Controller
      */
     public function index()
     {
-        //
+        $articles = Article::with('user')->get();
+        return view('pages.contributor.articles.index', ['articles' => $articles]);
     }
 
     /**
@@ -23,7 +27,7 @@ class ArticleController extends Controller
      */
     public function create()
     {
-        //
+        return view('pages.contributor.articles.create');
     }
 
     /**
@@ -34,7 +38,23 @@ class ArticleController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // $input = $request->all();
+        // $user_id = Article::user()->articles();
+
+        // $create = Article::create([
+        //     'title' => $request->title,
+        //     'content' => $request->content,
+        //     'user_id' => Article::user()->user_id,
+        // ]);
+
+        $data = new Article;
+        $data->title = $request->title;
+        $data->content = $request->content;
+
+        // ini secara otomatis akan mengisi 'user_id' dengan Auth::user()->id
+        Auth::user()->articles()->save($data); 
+
+        return redirect()->route('articles.index')->with('status', 'Article created.');
     }
 
     /**
@@ -56,7 +76,12 @@ class ArticleController extends Controller
      */
     public function edit($id)
     {
-        //
+        
+        $item = Article::findOrfail($id);
+
+        return view('pages.contributor.articles.edit', [
+                    'item' => $item
+                ]);
     }
 
     /**
